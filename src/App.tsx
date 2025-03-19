ca3c6ec85f70ca9fd2e883a7230e5291d674f388
Magda <wozna.magda@gmail.com>
2025-03-19 12:43:54 +0100

    feat(form): add form ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import { Book } from "./types";
+import { InputForm } from "./components/custom/single-input-form";
 
 function App() {
   const [books, setBooks] = useState<Book[]>([]);
@@ -19,14 +20,17 @@ function App() {
   }, []);
 
   return (
-    <>
-      <h1>📚 Books from strapi:</h1>
-      <ul className="my-10">
+    <div className="flex flex-col items-center gap-12 mt-20">
+      <h1>📚 Books from strapi</h1>
+      <h2>Add another one:</h2>
+      <InputForm />
+      <h2 className="mt-6">Books already read:</h2>
+      <ul>
         {books.map((book) => (
           <li key={book.Title}>"{book.Title}"</li>
         ))}
       </ul>
-    </>
+    </div>
   );
 }
 
---
+++ src/components/custom/single-input-form.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { toast } from "sonner";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const FormSchema = z.object({
+  title: z.string().min(1, {
+    message: "Title must be at least 1 character.",
+  }),
+});
+
+export function InputForm() {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      title: "",
+    },
+  });
+
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    toast("Form submitted");
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 space-y-6">
+        <FormField
+          control={form.control}
+          name="title"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Book title</FormLabel>
+              <FormControl>
+                <Input
+                  placeholder="e.g. Harry Potter and The Goblet of Fire"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+}
